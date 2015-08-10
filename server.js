8fcca1896e744b65adb5daeb466c0a4070ed5fb2
liuuki <iki.s.liuu@gmail.com>
2015-08-10 19:06:01 +0100

    final v5
    change server.js, create xls file directly in /data folder
    
    move all pic to /images folder

--- server.js
+++ server.js
@@ -2,19 +2,41 @@ var fs = require('fs');
 var http = require('http');
 var url = require('url') ;
 var time = new Date();
-var myfile = Date.now() + '.txt';
+var myfile = './data/' + Date.now() + '.xls';
+var clickDataStr = "";
+
+//Create header ...
+var head = "Click" + "\t" + "Time" + "\n";
+fs.appendFile(myfile, head, function (err) {
+                               if (err) return console.log(err);
+                               console.log('ADD LOG > ' + myfile);
+                               });
 
 http.createServer(function (req, res) {
                   var queryObject = url.parse(req.url,true).query;
                   console.log(queryObject);
-                  if(queryObject!={})
-                  fs.appendFile(myfile, JSON.stringify(queryObject), function (err) {
+                  if(queryObject!={}){
+                        clickDataStr = getData(queryObject);
+                        fs.appendFile(myfile, clickDataStr, function (err) {
                                if (err) return console.log(err);
                                console.log('ADD LOG > ' + myfile);
                                });
-                  
+                  }                  
                   res.writeHead(200);
                   res.end('Feel free to add query parameters to the end of the url');
                   }).listen(8090);
 
+function getData(rawData){
+      var dataStr = "";
+      var strData = JSON.stringify(rawData);
+      strData = strData.slice(1, strData.length - 1);
+      var data = strData.split(",");
+      var order = data[0].split(":");
+      var orderData = order[1].slice(1, order[1].length - 1);
+      var time = data[1].split(":");
+      var timeData = time[1].slice(1, time[1].length - 1);
+      dataStr = dataStr + orderData + "\t";
+      dataStr = dataStr + timeData + "\n";
+      return dataStr;
+}
 
